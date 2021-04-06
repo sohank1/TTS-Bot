279e0eb1649f4a74adbf8d78ab798f093c74d3c6
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-04-06 13:11:40 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
+//@ts-nocheck
 import "dotenv/config";
-import "./client/Client";
+import { client } from "./client/Client";
 import { join } from "path";
 import { BaseCommand } from "./commands/baseCommand/BaseCommand";
 import { BaseEvent } from "./events/baseEvent/BaseEvent";
 
 BaseCommand.register(join(__dirname, "commands"));
 BaseEvent.register(join(__dirname, "events"));
+
+
+client.api.applications(client.user.id).commands.post({
+  data: {
+    name: "ping",
+    description: "Gets your ping to TTS Bot",
+  },
+});
+
+client.ws.on("INTERACTION_CREATE", async interaction => {
+  // Do stuff here - interaction is an InteractionResponse object. To get the name, use interaction.data.name
+  // In particular, the values you passed to the interaction when creating it will be passed back here
+  
+  if (interaction.data.name === "ping") {
+    client.api.interactions(interaction.id, interaction.token).callback.post({
+      data: {
+      type: 3,
+      data: {
+       content: client.ws.ping,
+       },
+      },
+    });
+  }
+});
