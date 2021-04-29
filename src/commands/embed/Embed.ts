4ad15a097c4b489f206a541fd1a86f83741f61ed
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2021-04-29 11:00:30 -0400

    Update Embed.ts

--- src/commands/embed/Embed.ts
+++ src/commands/embed/Embed.ts
@@ -24,15 +24,18 @@ export default class EmbedCommand extends BaseCommand {
             .sendTo(this.message.channel);
         
         require("axios").get("https://fortnite-api.com/v2/cosmetics/br").then(r => {
-const missing = [];
- r.data.data.forEach(c => {
-const date = new Date(c.shopHistory && c.shopHistory[c.shopHistory.length - 1])
-const differenceInDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
-if (differenceInDays >= 300)  missing.push(c)
-})
+        const missing = [];
+        r.data.data.forEach(c => {
+            const date = new Date(c.shopHistory && c.shopHistory[c.shopHistory.length - 1])
+            const differenceInDays = (date.getTime() - Date.now()) / (1000 * 3600 * 24);
+            if (differenceInDays >= 300)  missing.push(c)
+        })
 
-missing.forEach(e => this.message.channel.send(`Missing ${e.name}. Last Seen: ${new Date(c.shopHistory[c.shopHistory.length - 1]).toLocaleString()}`))
-})
+    missing.forEach(e => this.message.channel.send(`Missing ${e.name}. Last Seen: ${new Date(c.shopHistory[c.shopHistory.length - 1]).toLocaleString()}`))
+        })
+        
+        
+        
     }
 
 }
